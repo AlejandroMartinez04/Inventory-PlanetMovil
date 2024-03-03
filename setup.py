977a5d7fc6238746d4e2f5e56f7943a95cfc1957
alejandro <diegomartinez1101@gmail.com>
2024-03-02 23:33:38 -0500

    Fix login using users

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-import sys
-import os
-
 from cx_Freeze import setup, Executable
 
-files = ['assets','databaseProducts.db','pys6_msgBoxes','credentials.txt']
+files = ['assets','databaseProducts.db','pys6_msgBoxes']
 
 exe = Executable(script="app.py", base="win32GUI")
 
 setup(
-    name="Inventario PlanetMovil",
-    version="4.5",
-    description="Inventario y contabilidad para planet movil",
+    name="Inventario",
+    version="4.6",
+    description="Inventario y contabilidad",
     author="Diego Martinez",
     author_email='diegomartinez1101@gmail.com',
     options={'build_exe': {'include_files': files}},
 
     # executables=[Executable("app.py")],
-    # packages=["view", "model", "assets", "uifiles", "controller", "pys6_msgBoxes"],
+    # packages=["view", "model", "assets", "controller", "pys6_msgBoxes"],
 
     executables=[exe],
-    packages=['assets','databaseProducts.db','pys6_msgBoxes','credentials.txt']
+    packages=['assets','databaseProducts.db','pys6_msgBoxes']
 )
 
 # python setup.py build_exe --zip-include-packages=encodings,PySide6 -----COMANDO PARA CREAR .EXE
@@ -28,3 +25,4 @@ setup(
 
 # auto-py-to-exe
 
+# pyinstaller --onedir .\app.py --add-data "assets;assets" --add-data "pys6_msgBoxes;pys6_msgBoxes" --name inventario --icon .\assets\sale.ico --console
