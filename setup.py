cad8f9a7df2d19e1afb9f328c851143ca54df676
alejandro <diegomartinez1101@gmail.com>
2024-03-02 23:33:59 -0500

    test

--- setup.py
+++ setup.py
@@ -12,17 +12,9 @@ setup(
     author_email='diegomartinez1101@gmail.com',
     options={'build_exe': {'include_files': files}},
 
-    # executables=[Executable("app.py")],
-    # packages=["view", "model", "assets", "controller", "pys6_msgBoxes"],
-
     executables=[exe],
     packages=['assets','databaseProducts.db','pys6_msgBoxes']
 )
 
 # python setup.py build_exe --zip-include-packages=encodings,PySide6 -----COMANDO PARA CREAR .EXE
 
-# python setup.py bdist_msi ---- CREA EL .EXE PARA INSTALACION DEL PROYECTO
-
-# auto-py-to-exe
-
-# pyinstaller --onedir .\app.py --add-data "assets;assets" --add-data "pys6_msgBoxes;pys6_msgBoxes" --name inventario --icon .\assets\sale.ico --console
