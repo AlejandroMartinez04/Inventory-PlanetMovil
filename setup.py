787e3c0fa400ba2b6a6880354ea852b49d947ba9
DiegoMartinez <diegomartinez1101@mail.com>
2025-03-06 18:58:28 -0500

    New setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
-files = ['assets','databaseProducts.db','pys6_msgBoxes']
+files = ['assets','databaseProducts.db','pys6_msgBoxes','.env']
 
 exe = Executable(script="app.py", base="win32GUI")
 
@@ -10,11 +10,17 @@ setup(
     description="Inventario y contabilidad",
     author="Diego Martinez",
     author_email='diegomartinez1101@gmail.com',
-    options={'build_exe': {'include_files': files}},
-
+    options={
+        'build_exe': {
+            'include_files': files,
+            'excludes': ['msilib']
+        }
+    },
     executables=[exe],
-    packages=['assets','databaseProducts.db','pys6_msgBoxes']
+    packages=['assets', 'databaseProducts.db', 'pys6_msgBoxes']
 )
 
 # python setup.py build_exe --zip-include-packages=encodings,PySide6 -----COMANDO PARA CREAR .EXE
+
+# pyinstaller --onefile --add-data "assets;assets" --add-data ".env;." --add-data "pys6_msgBoxes;pys6_msgBoxes" app.py ----COMANDO PARA CREAR .EXE
 
